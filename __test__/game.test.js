779465a0898ddf2c52ded3040bbd80dfa73999a4
nachoborente <127313115+nachoborente@users.noreply.github.com>
2023-06-05 11:08:47 +0200

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,13 +2,16 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      done()
-    })
-  })
-})
+  it('Contains the compiled JavaScript', async () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile('./public/main.js', 'utf8', (err, data) => {
+        expect(err).toBe(null);
+        expect(data).toMatchSnapshot()
+        resolve();
+      });
+    });
+  });
+});
 
 describe('Game', () => {
   let game, p1, p2
